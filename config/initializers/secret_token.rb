7185b084dd436c14cd7e2cd670b8673e04a45555
Takeshi Sato <master.extelligence@gmail.com>
2011-07-26 22:56:52 +0900

    create project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaypalSample::Application.config.secret_token = 'cfd0285fd7e479abef570690163acf958094723073bf4f66cabf932b0868318bff72d7f998830d548202267348ed8e8f810685032bc02f68850541199fc9bfb4'
